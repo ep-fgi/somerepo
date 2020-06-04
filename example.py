118c7ba9f8eb7aa3858bfe3b7bbd73021f0f62d5
ep-fgi <ep-fgi@users.noreply.github.com>
2020-06-04 11:43:42 +0300

    restore function subtract; fixes #1

--- example.py
+++ example.py
@@ -8,8 +8,7 @@ def test_add():
 
 
 def subtract(a, b):
-    return a - b  # <--- fix this in step 8, ep - edit
-
+    return a - b  # <--- fix this in step 8 - ep - edit
 
 # uncomment the following test in step 5
 def test_subtract():
